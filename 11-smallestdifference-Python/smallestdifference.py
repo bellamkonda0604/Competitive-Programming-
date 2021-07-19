6fc24566efb8cbf8fd22b28c924210e387ff7a66
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-19 18:44:47 +0530

    solved

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -6,4 +6,29 @@
 
 def smallestdifference(a):
 	# Your code goes here
-	pass
+	# l = []
+	# if a == []:
+	# 	return -1
+	# for i in a:
+	# 	if i < 0:
+	# 		x = abs(i)
+	# 		l.append(x)
+	# 	else:
+	# 		l.append(i)
+	# a = sorted(l)
+	# print(a)
+	# for i in a:
+	# 	diff = abs(a[1] - a[0])
+	# print(diff)
+	a.sort()
+	diff = 0
+	if(len(a) == 0):
+		return -1
+	smalldiff = abs(a[0] - a[1])
+	for i in range(len(a)-1):
+		diff = abs(a[i] - a[i+1])
+		if(diff < smalldiff):
+			smalldiff = diff
+	return smalldiff
+
+# print(smallestdifference([1,1,2]))
