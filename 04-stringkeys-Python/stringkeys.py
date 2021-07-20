ee17f6c5a2c463fbed4b8cfc78a35196e1041723
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-20 09:10:36 +0530

    solved

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -14,38 +14,48 @@ class HashTable(object):
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
+        # hashvalue = self.calculate_hash_value(string)
+        # if self.table[hashvalue] != None:
+        #     print("hi")
+        #     self.table[hashvalue] = (string)
+        #     print(self.table)
+        # else:
+        #     self.table[hashvalue] = [string]
+        # # print(hashvalue)
+        # return hashvalue
         hashvalue = self.calculate_hash_value(string)
-        if self.table[hashvalue] != None:
-            print("hi")
-            self.table[hashvalue] = (string)
-            print(self.table)
-        else:
-            self.table[hashvalue] = [string]
-        # print(hashvalue)
-        return hashvalue
+        self.table.insert(hashvalue, string)
+        return -1
 
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        hashvalue = self.calculate_hash_value(string)
+        # hashvalue = self.calculate_hash_value(string)
         # print(hashvalue)
         # print(self.table)
         # print(hash)
         # print(self.table[hashvalue])
         # print(string)
-        if string in self.table[hashvalue]:
-            return hashvalue
-        return -1
+        # if string in self.table[hashvalue]:
+        #     return hashvalue
+        # return -1
+        hash = self.calculate_hash_value(string)
+        if(self.table[hash] == None or self.table[hash]!=string):
+            return -1
+        else:
+            return hash
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        hashvalue = ord(string[0])*100 + ord(string[1])
-        # print(hashvalue)
-        return hashvalue
+        # hashvalue = ord(string[0])*100 + ord(string[1])
+        # # print(hashvalue)
+        # return hashvalue
+        Hashvalue = (ord(string[0])*100)+ ord(string[1])
+        return Hashvalue
 
 # print(HashTable('UDACITY'))
 
