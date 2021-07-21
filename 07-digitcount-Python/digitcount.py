b47105d6af93410e0bd96f2002f8f4e400f1b170
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-21 18:46:08 +0530

    solved

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -7,4 +7,13 @@
 
 def digitcount(n):
 	# your code goes here
-	pass
+	n = abs(n)
+	count = 0
+	if n == 0:
+		return 1
+	while n > 0:
+		count += 1
+		n = n // 10
+	return count
+
+print(digitcount(521469784213))
