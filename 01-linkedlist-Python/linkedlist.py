a00f9f38a4d7bf45f4dc5d5f13f25fe4162fc885
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-22 21:03:19 +0530

    solved

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -32,18 +32,46 @@ class LinkedList(object):
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
+        count = 1
+        current = self.head
+        if position < 1:
+            return None
+        while current and count <= position:
+            if count == position:
+                return current
+            current = current.next
+            count += 1
+        return None
         
-    
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
-    
+        count = 1
+        current = self.head
+        if position > 1:
+            while current and count < position:
+                if count == position - 1:
+                    new_element.next = current.next
+                    current.next = new_element
+                current = current.next
+                count += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
