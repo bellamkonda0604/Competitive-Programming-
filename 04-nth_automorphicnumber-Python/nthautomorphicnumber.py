c1b593248895da5ce5ec7adcd21f5bdcd6089eb9
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-23 10:26:37 +0530

    tried

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,6 +3,21 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
+
+
+
+def isautomorphicnumber(n):
+	if n == (n ** 2) % 10:
+		return True
+	return False
+
+
 def nthautomorphicnumbers(n):
 	# Your code goes here
-	pass
+	count = 1
+	i = 1
+	while count < n:
+		i += 1
+		if isautomorphicnumber(n):
+			count += 1
+	return i
