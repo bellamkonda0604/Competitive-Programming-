9e95f031aaeb1894519020c3bea4b4a44f0f2055
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-23 16:46:17 +0530

    solved

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,11 +3,15 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
-
-
+def digitcount(n):
+	count = 0
+	while (n>0):
+		count += 1
+		n = n//10
+	return count
 
 def isautomorphicnumber(n):
-	if n == (n ** 2) % 10:
+	if n == (n ** 2) % (10 ** digitcount(n)):
 		return True
 	return False
 
@@ -15,9 +19,9 @@ def isautomorphicnumber(n):
 def nthautomorphicnumbers(n):
 	# Your code goes here
 	count = 1
-	i = 1
+	i = 0
 	while count < n:
 		i += 1
-		if isautomorphicnumber(n):
+		if isautomorphicnumber(i):
 			count += 1
 	return i
