bacc31d05041676c1dbec0444e5a0c849346660f
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-26 11:07:50 +0530

    tried

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -13,24 +13,46 @@ class BinaryTree(object):
         Return True if the find_val is in the tree and False otherwise.
         """
         # Your code goes here
-        pass
+        print(self.data)
+        if self.value == find_val:
+            return True
+        if find_val < self.value:
+            if self.left:
+                self.left.search(find_val)
+            else:
+                return False    
+        if find_val > self.value:
+            if self.right:
+                self.right.search(find_val)
+            else:
+                return False   
 
     def print_tree(self):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
-        pass
+        if self.left:
+            self.left.print_tree()
+        if self.right:
+            self.right.print_tree()
 
     def preorder_search(self, start, find_val):
         """
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        pass
+        res = []
+        if start:
+            res.append(start.value)
+            res = res + self.PreorderTraversal(start.left)
+            res = res + self.PreorderTraversal(start.right)
+        return res
 
     def preorder_print(self, start, traversal):
         """
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
-        pass
+        return start.preorder_search(start)
+
+print(BinaryTree(20))
