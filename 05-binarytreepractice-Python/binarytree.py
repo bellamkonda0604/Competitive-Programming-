cb56408acb1d439aba194c898f856bbced248e7a
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-27 18:12:14 +0530

    solved

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -13,46 +13,44 @@ class BinaryTree(object):
         Return True if the find_val is in the tree and False otherwise.
         """
         # Your code goes here
-        print(self.data)
-        if self.value == find_val:
-            return True
-        if find_val < self.value:
-            if self.left:
-                self.left.search(find_val)
-            else:
-                return False    
-        if find_val > self.value:
-            if self.right:
-                self.right.search(find_val)
-            else:
-                return False   
+        if self.root is not None:
+            return self.find(find_val, self.root)
+        else:
+            return False
 
-    def print_tree(self):
+
+    def find(self, val, node):
+        if node:
+            if val == node.value:
+                return True
+            else:
+                return False
+            return self.find(val, node.left)
+            return self.find(val, node.right) 
+
+    def print_tree(self, node):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
-        if self.left:
-            self.left.print_tree()
-        if self.right:
-            self.right.print_tree()
+        if node is not None:
+            self.print_tree(node.left)
+            print(str(node.value) + ' ')
+            self.print_tree(node.right)
+        
 
     def preorder_search(self, start, find_val):
         """
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        res = []
-        if start:
-            res.append(start.value)
-            res = res + self.PreorderTraversal(start.left)
-            res = res + self.PreorderTraversal(start.right)
-        return res
+        if self.root is not None:
+            self.print_tree(self.root)
+        
 
     def preorder_print(self, start, traversal):
         """
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
-        return start.preorder_search(start)
-
-print(BinaryTree(20))
+        if self.root is not None:
+            self.print_tree(self.root)
