cae6ab166d4e785a9c2758f726e4d11381e17c28
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-28 09:35:00 +0530

    solved

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -8,18 +8,34 @@ class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
-    def insert(self, new_val, root):
+    def insert(self, new_val):
         # Your code goes here
-        if root is None:
-            return new_val
+        # if root is None:
+        #     return new_val
+        # else:
+        #     if root.value == new_val:
+        #         return root
+        #     elif root.value < new_val:
+        #         return find(new_val, root.right)
+        #     else:
+        #         return find( new_val, root.left)
+        # return root
+        newNode = Node(new_val)
+        if self.root is None:
+            self.root = newNode
         else:
-            if root.value == new_val:
-                return root
-            elif root.value < new_val:
-                return find(new_val, root.right)
+            current = self.root
+            parent = self.root
+            while current != None:
+                parent = current
+                if new_val < current.value:
+                    current = current.left
+                else:
+                    current = current.right
+            if new_val < parent.value:
+                parent.left = newNode
             else:
-                return find( new_val, root.left)
-        return root
+                parent.right = newNode
 
     def printSelf(self, node):
         # Your code goes here
@@ -31,21 +47,29 @@ class BST(object):
         
     def search(self, find_val):
         # Your code goes here
-        if self.root is not None:
-            return self.find(find_val, self.root)
-        else:
-            return None
+        # if self.root is not None:
+        #     return self.find(find_val, self.root)
+        # else:
+        #     return None
 
-    def find(self, val, node):
-        if node:
-            if val == node.value:
-                return True
+        while self.root != None:
+            if self.root.value == find_val:
+                return True 
+            if self.root.value < find_val:
+                self.root = self.root.right
             else:
-                return False
-            return self.find(val, node.left)
-            return self.find(val, node.right)
-        elif val < node.value and node.left is not None:
-            return self.find(val, node.left)
-        elif val > node.value and node.right is not None:
-            return self.find(val, node.right)
+                self.root = self.root.left
+        return False
+    # def find(self, val, node):
+    #     if node:
+    #         if val == node.value:
+    #             return True
+    #         else:
+    #             return False
+    #         # return self.find(val, node.left)
+    #         # return self.find(val, node.right)
+    #     elif val < node.value and node.left is not None:
+    #         return self.find(val, node.left)
+    #     elif val > node.value and node.right is not None:
+    #         return self.find(val, node.right)
 
