91b59f9d2e89718173ff7b35f10ddd39449c87b4
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-07-30 16:24:49 +0530

    solved

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,27 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def tidyNumber(n):
+    n = abs(n)
+    temp = n
+    x = 0
+    y = 0
+    while(temp > 0):
+        x = temp % 10
+        temp = temp // 10
+        y = temp % 10
+        if (x < y):
+            return False
+    return n
+
 def fun_nth_tidynumber(n):
-    return 0
+    count = 0
+    i = 1
+    while (count <= n):
+        if (tidyNumber(i)):
+            count = count + 1
+        i = i + 1
+    return i-1
+
+
+# print(fun_nth_tidynumber(15))
