c71c8eb892a89ef27b2c7ba49d975024310d131e
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-08-02 20:09:19 +0530

    solved

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,36 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+
+def isprime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def squareofdigits(n):
+	res = 0
+	while (n > 0):
+		res += (n % 10)**2
+		n = n // 10
+	return res
+
+def ishappynumber(n):
+	if n <= 0:
+		return False
+	while (1):
+		if n == 1:
+			return True
+		if n == 4:
+			return False
+		else:
+			n = squareofdigits(n)
+	return False
+ 
 def ishappyprimenumber(n):
     # Your code goes here
-    pass
+    if isprime(n) and ishappynumber(n):
+        return True
+    return False
