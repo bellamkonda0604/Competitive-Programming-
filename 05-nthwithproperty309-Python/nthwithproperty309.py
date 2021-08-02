42a01dcdc80ca02e13fd261e45718a2f64bd40bf
bellamkonda0604 <yashwanth.konda143@gmail.com>
2021-08-02 20:00:09 +0530

    solved

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -3,7 +3,28 @@
 # digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
+import re
+
+def digits(n):
+	return len(set("".join(re.findall(r'([\d])', str(n))))) == 10
+
+def withproperty309(n):
+	n = n ** 5
+	# if len(set("".join(re.findall(r'([\d])', str(n))))) == 10:
+	# 	return True
+	# return False
+	if digits(n):
+		return True
+	return False
 
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	count = 0
+	i = 0
+	while count <= n:
+		i += 1
+		if withproperty309(i):
+			count += 1
+	return i
+
+# print(nthwithproperty309(0))
